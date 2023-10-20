b3c0ff4c17bee7b22e632194c94cc1cb5f8ad728
seema2404 <seema.kum@batchservice.com>
2023-10-20 23:03:11 +0530

    initial code

---
+++ admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="login.css" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+</head>
+<body>
+    <div class="login-form">
+        <h2>ADMIN LOGIN PANEL</h2>
+        <form action="">
+            <div>
+                <i class="fa-solid fa-user"></i>
+                <input type="text" name="" placeholder="Username" required>
+            </div>
+            <div>
+                <i class="fa-solid fa-lock"></i>
+                <input type="password" placeholder="Password" required>
+            </div>
+            <button type="submit">LOGIN</button>
+            <div>
+                <a href="#">Back to Home</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <div  class="sidebar">
+        <div class="logo"></div>
+        <ul class="main">
+            <li>Dashboard</li>
+            <li>Manage Users</li>
+            <li>Manage Booking</li>
+            <li>Profile</li>
+            <li>Review and Rating</li>
+            <li>Logout</li>
+        </ul>
+    </div>
+</body>
+</html>
